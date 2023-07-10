d1942995973eaa73e548a6eba49ae3210bb51c05
ArooiD <125826490+ArooiD@users.noreply.github.com>
2023-07-10 16:27:13 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,30 +1,26 @@
-# This workflow uses actions that are not certified by GitHub.
-# They are provided by a third-party and are governed by
-# separate terms of service, privacy policy, and support
-# documentation.
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
-# GitHub recommends pinning actions to a commit SHA.
-# To get a newer version, you will need to update the SHA.
-# You can also reference a tag or branch, but the action may change without warning.
+import PackageDescription
 
-name: Swift
-
-on: [push]
-
-jobs:
-  build:
-    name: Swift ${{ matrix.swift }} on ${{ matrix.os }}
-    strategy:
-      matrix:
-        os: [ubuntu-latest, macos-latest]
-        swift: ["5.2", "5.3"]
-    runs-on: ${{ matrix.os }}
-    steps:
-      - uses: swift-actions/setup-swift@65540b95f51493d65f5e59e97dcef9629ddf11bf
-        with:
-          swift-version: ${{ matrix.swift }}
-      - uses: actions/checkout@v3
-      - name: Build
-        run: swift build
-      - name: Run tests
-        run: swift test
+let package = Package(
+    name: "SDK",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "SDK",
+            targets: ["SDK"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "SDK",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "SDKTests",
+            dependencies: ["SDK"]),
+    ]
+)
